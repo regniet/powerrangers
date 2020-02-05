e5d5ee8c80e7cdf8ea9051ff94c8fd0b746eab2d
Regniet <alain.regniet@accenture.com>
2020-02-05 09:08:06 +0100

    Ein Projekt von powerrangers initialisiert

---
+++ src/main/java/com/acn/powerranger/PowerrangerApplication.java
@@ -0,0 +1,13 @@
+package com.acn.powerranger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerrangerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PowerrangerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/acn/powerranger/PowerrangerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acn.powerranger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowerrangerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
